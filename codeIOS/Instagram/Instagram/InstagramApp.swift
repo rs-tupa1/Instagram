14a0ce421879660e5dc323cfbb0723c6e85371ce
tupa1 <tupa1@runsystem.net>
2021-12-16 10:07:08 +0700

    update podfile

---
+++ codeIOS/Instagram/Instagram/InstagramApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramApp.swift
+//  Instagram
+//
+//  Created by GMO on 12/13/21.
+//
+
+import SwiftUI
+
+@main
+struct InstagramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ codeIOS/Instagram/Instagram/main.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by GMO on 12/3/21.
+//
+
+import SwiftUI
+
+@main
+struct Instagram: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginScreen()
+        }
+    }
+}
